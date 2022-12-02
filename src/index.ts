3b07aad2fd40bc80b8845515d65ef03815c5a41f
mrSviridovD <sd.kpuca@gmail.com>
2022-12-02 20:56:13 +0400

    +2 api get

--- src/index.ts
+++ src/index.ts
@@ -5,26 +5,26 @@ const port = process.env.PORT || 3000
 
 const videos = [
     {
-        "id": 1,
-        "title": "Звёздные войны: Эпизод 1 – Скрытая угроза",
-        "author": "Джордж Лукас",
-        "canBeDownloaded": true,
-        "minAgeRestriction": 16,
-        "createdAt": "1991-05-19",
-        "publicationDate": "2022-12-02T16:09:48.979Z",
-        "availableResolutions": [
+        id: 1,
+        title: "Звёздные войны: Эпизод 1 – Скрытая угроза",
+        author: "Джордж Лукас",
+        canBeDownloaded: true,
+        minAgeRestriction: 16,
+        createdAt: "1991-05-19",
+        publicationDate: "2022-12-02T16:09:48.979Z",
+        availableResolutions: [
         "P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"
         ]
     },
     {
-        "id": 2,
-        "title": "Звёздные войны: Эпизод 1 – Скрытая угроза",
-        "author": "Джон Лассетер",
-        "canBeDownloaded": true,
-        "minAgeRestriction": 3,
-        "createdAt": "2005-08-16",
-        "publicationDate": "2022-12-02T11:09:48.979Z",
-        "availableResolutions": [
+        id: 2,
+        title: "Звёздные войны: Эпизод 1 – Скрытая угроза",
+        author: "Джон Лассетер",
+        canBeDownloaded: true,
+        minAgeRestriction: 3,
+        createdAt: "2005-08-16",
+        publicationDate: "2022-12-02T11:09:48.979Z",
+        availableResolutions: [
             "P360", "P480", "P720", "P1080"
         ]
     },
@@ -41,7 +41,15 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
-app.get('/videos',(req:Request, res:Response) => {
-
+app.get('/videos/:id',(req:Request, res:Response) => {
+    let video = videos.find(v => v.id === +req.params.id)
+    if(video){
+        res.send(video)
+    }else {
+        res.send(404)
+    }
+})
+app.get('/videos',(req: Request,res:Response) => {
+    res.send(videos)
 })
 
