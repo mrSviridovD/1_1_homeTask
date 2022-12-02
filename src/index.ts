54e1481a82fdd35fc88abf5d0b17742fd5fc79f5
mrSviridovD <sd.kpuca@gmail.com>
2022-12-02 21:47:22 +0400

    + api delete

--- src/index.ts
+++ src/index.ts
@@ -52,4 +52,19 @@ app.get('/videos/:id',(req:Request, res:Response) => {
 app.get('/videos',(req: Request,res:Response) => {
     res.send(videos)
 })
+app.delete('/videos/:id',(req:Request,res:Response) => {
+    for (let i = 0; i < videos.length; i++) {
+        if(videos[i].id === +req.params.id){
+            videos.splice(i,1);
+            res.send(204)
+            return
+        }else {
+            res.send(404)
+            return
+        }
+    }
 
+
+
+
+})
